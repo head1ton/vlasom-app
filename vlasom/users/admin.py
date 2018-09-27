a8b8121b581b78bd55f396787845674d26d3f0a8
SEUNGMIN KIM <smk950226@naver.com>
2018-09-28 02:08:11 +0900

    user admin 수정

--- vlasom/users/admin.py
+++ vlasom/users/admin.py
@@ -12,6 +12,40 @@ class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "name", "is_superuser"]
-    search_fields = ["name"]
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'name', 'nickname', 'birth_year', 'birth_month', 'birth_day', 'password1', 'password2', 'is_verified'),
+        }),
+    )
+    fieldsets = [
+        ('개인정보', {'fields': [
+            'username',
+            'name',
+            'nickname',
+            'profile_image',
+            'email',
+            'join_channel',
+            'gender',
+            'birth_year',
+            'birth_month',
+            'birth_day',
+            'is_verified',
+        ]}),
+        ('권한', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'password'
+        )}),
+        ('팔로잉', {'fields': (
+            'follower',
+            'following'
+        )})
+    ]
+    list_display = ['id', 'username', 'email', 'nickname', 'is_verified', 'is_staff']
+    list_display_links = ['id', 'username']
+    search_fields = ['username', 'email', 'nickname']
+    ordering = ['-id']
+    list_filter = ['is_verified', 'is_active']
+    
