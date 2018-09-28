1e5d9ce39d6404d1a8c60cde68a1d61b3c88b52e
SEUNGMIN KIM <smk950226@naver.com>
2018-09-28 20:54:37 +0900

    comments / likes view 생성

--- vlasom/images/urls.py
+++ vlasom/images/urls.py
@@ -5,4 +5,6 @@ app_name = 'images'
 
 urlpatterns = [
     path('all/', views.ListAllImages.as_view(), name = 'all_images'),
+    path('comments/', views.ListAllComments.as_view(), name = 'all_comments'),
+    path('likes/', views.ListAllLikes.as_view(), name = 'all_likes'),
 ]
