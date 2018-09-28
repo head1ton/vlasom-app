fc5334cf4b528c35f377b94d6a2ea5e4e6b7c98d
SEUNGMIN KIM <smk950226@naver.com>
2018-09-29 02:29:05 +0900

    like on an image

--- vlasom/images/urls.py
+++ vlasom/images/urls.py
@@ -5,4 +5,5 @@ app_name = 'images'
 
 urlpatterns = [
     path('', views.Feed.as_view(), name = 'feed'),
+    path('<int:image_id>/like/', views.LikeImage.as_view(), name = 'like_image'),
 ]
--- vlasom/images/views.py
+++ vlasom/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 class Feed(APIView):
@@ -19,3 +20,21 @@ class Feed(APIView):
         sorted_list = sorted(image_list,key=lambda x: x.created_at, reverse=True)
         serializer = serializers.ImageSerializer(sorted_list, many = True)
         return Response(serializer.data)
+
+
+class LikeImage(APIView):
+    def get(self, request, image_id, format = None):
+
+        try:
+            found_image = models.Image.objects.get(id = image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            pre_existing_like = models.Like.objects.get(user = request.user, image = found_image)
+            pre_existing_like.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Like.DoesNotExist:
+            new_like = models.Like.objects.create(user = request.user, image = found_image)
+            new_like.save()
+
+            return Response(status=status.HTTP_201_CREATED)
