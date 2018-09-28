daa68bdaacc00d69388f57186d7f464222500278
SEUNGMIN KIM <smk950226@naver.com>
2018-09-29 03:19:53 +0900

    user recommendation

---
+++ vlasom/users/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from . import models
+
+User = get_user_model()
+
+class ExploreUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['profile_image', 'username', 'nickname', 'name']
