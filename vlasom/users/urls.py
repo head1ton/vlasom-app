1b904a03d80d09f3951ed6d78a149cc5cbee0332
SEUNGMIN KIM <smk950226@naver.com>
2018-09-30 01:45:02 +0900

    username 검색

--- vlasom/users/urls.py
+++ vlasom/users/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("explore/", views.ExploreUsers.as_view(), name="explore_users"),
     path('<int:user_id>/follow/', views.FollowUser.as_view(), name = 'follow_user'),
     path('<int:user_id>/unfollow/', views.UnFollowUser.as_view(), name = 'unfollow_user'),
-    path('<str:username>/', views.UserProfile.as_view(), name = 'user_profile'),
     path('<str:username>/follower/', views.UserFollower.as_view(), name = 'user_follower'),
     path('<str:username>/following/', views.UserFollowing.as_view(), name = 'user_following'),
+    path('search/', views.Search.as_view(), name = 'user_search'),
+    path('<str:username>/', views.UserProfile.as_view(), name = 'user_profile'),
 ]
