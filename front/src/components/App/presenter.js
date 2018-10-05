4caa3fd0857d85f86b3f9cdbb819b2ce32d791db
SEUNGMIN KIM <smk950226@naver.com>
2018-10-06 02:13:11 +0900

    login router

--- front/src/components/App/presenter.js
+++ front/src/components/App/presenter.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import styles from 'components/App/styles.scss';
 import Footer from 'components/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App} >
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = props => [
+    props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublickRoutes key={2} />,
+    <Footer key={3} />
+]
+
+const PrivateRoutes = props => (
+    <Switch>
+        <Route exact path='/' render={() => 'feed'} />
+        <Route esact path='/explore' render={() => 'explore'} />
+    </Switch>
+)
+
+const PublickRoutes = props => (
+    <Switch>
+        <Route exact path='/' render={() => 'login'} />
+        <Route esact path='/forgot' render={() => 'password'} />
+    </Switch>
+)
 
 export default App;
