81c45172b35517bfb9d240fb5f0a5165f6635124
SEUNGMIN KIM <smk950226@naver.com>
2018-10-07 16:46:27 +0900

    translate footer

--- front/src/translations.js
+++ front/src/translations.js
@@ -1,5 +1,16 @@
 export const translations = {
-    es: {
-        'Login': 'Iniciar Sesion'
+    kr: {
+        'Login': '로그인',
+        'Signup': '회원가입',
+        'About Us': '연락처',
+        'Support': '고객지원',
+        'Blog': '블로그',
+        'Press': '매체',
+        'API': '기술지원',
+        'Jobs': '채용',
+        'Privacy': '개인정보처리방침',
+        'Terms': '이용약관',
+        'Directory': '디렉토리',
+        'Language': '언어'
     }
 }
