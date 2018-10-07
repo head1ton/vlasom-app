3eaa3de9c807c61fc356a2c4e310aeaa355ac41a
SEUNGMIN KIM <smk950226@naver.com>
2018-10-07 17:05:55 +0900

    make translation file automatically

--- front/src/translations.js
+++ front/src/translations.js
@@ -1,16 +1,16 @@
 export const translations = {
-    kr: {
-        'Login': '로그인',
-        'Signup': '회원가입',
-        'About Us': '연락처',
-        'Support': '고객지원',
-        'Blog': '블로그',
-        'Press': '매체',
-        'API': '기술지원',
-        'Jobs': '채용',
-        'Privacy': '개인정보처리방침',
-        'Terms': '이용약관',
-        'Directory': '디렉토리',
-        'Language': '언어'
-    }
+  'kr': {
+    'About Us': '연락처',
+    'Support': '고객지원',
+    'Blog': '블로그',
+    'Press': '매체',
+    'API': '기술지원',
+    'Jobs': '채용',
+    'Privacy': '개인정보처리방침',
+    'Terms': '이용약관',
+    'Directory': '디렉토리',
+    'Language': '언어',
+  },
+  'options': {
+  },
 }
